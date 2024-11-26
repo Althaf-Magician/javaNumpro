98f249d6eb5cbc6abba01e3f656a20686c4ab0bb
Althaf-sg <althafbook@gamil.com>
2024-11-26 10:21:43 +0530

    intial commit

--- factor.java
+++ factor.java
@@ -9,9 +9,10 @@ public class factor
         for (int i = 1; i <=num; i++)
         {
             if (num%i==0) 
-            {
+            { 
                 System.out.println(i+" is the factor of "+num);    
             }    
         }    
+        sc.close();
     }    
 }

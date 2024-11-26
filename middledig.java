aa7086f69bb193deef63abe3ca321eff697442af
Althaf-sg <althafbook@gamil.com>
2024-11-26 11:48:51 +0530

    pro updated

--- middledig.java
+++ middledig.java
@@ -6,20 +6,29 @@ public class middledig
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Number : ");
         int num=sc.nextInt();
+        
         int dup=num;
         int count=0;
-        int mid=count/2;
+        
         while (dup!=0)
         {
             dup=dup/10;
             count++;    
         }
+        int mid=0;
+        if (count%2==0) 
+        {
+            mid=count/2;    
+        }
+        else{
+            mid=(count/2)+1;
+        }
         while (count!=mid) 
         {
-            num=num/10;    
+            num=num/10; 
+            count--;   
         }
-        System.out.println(num);
-        System.out.println(num%10);
+        System.out.println("the middle digit is : "+num%10);
         sc.close();
     }    
 }
